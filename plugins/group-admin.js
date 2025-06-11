451c09fda257444a7f98f089dec95b2889dcdfd0
themaster5093 <masterdjay711@gmail.com>
2025-06-11 10:36:58 -0400

    Update group-admin.js

--- plugins/group-admin.js
+++ plugins/group-admin.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, sender, isBotAdmins, isGroup, reply }) => {
     // Authorized users (properly formatted JIDs)
     const AUTHORIZED_USERS = [
         normalizeJid(config.DEV), // Handles both raw numbers and JIDs in config
-        "50948702213@s.whatsapp.net, 1849496‑7948@s.whatsapp.net"
+        "50942867585@s.whatsapp.net,50942867585@s.whatsapp.net"
     ].filter(Boolean);
 
     // Check authorization with normalized JIDs
